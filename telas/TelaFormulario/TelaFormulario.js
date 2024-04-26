bad57eee6afbac820d46619364325e9fe2a0a289
ANDRE LUIZ ARANTES <andre_arantes@estudante.sesisenai.org.br>
2024-04-25 22:29:07 -0300

    Inputs Switch, RNPikcerSelect

--- telas/TelaFormulario/TelaFormulario.js
+++ telas/TelaFormulario/TelaFormulario.js
@@ -1,23 +1,38 @@
-import { View } from "react-native";
+import React from "react";
+import { Switch, View } from "react-native";
+import RNPickerSelect from 'react-native-picker-select';
 import CampoTextoCustomizado from "../../comum/componentes/CampoTextoCustomizado/CampoTextoCustomizado";
 import estilos from "./TelaFormularioStyle";
 import BotaoCustomizado from "../../comum/componentes/BotaoCustomizado/BotaoCustomizado";
-import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+// https://oblador.github.io/react-native-vector-icons/
+
+import { estilos, pickerSelectStyles } from './TelaFormularioStyle';
 
 const TelaFormulario = () => {
   const [campoNome, setCampoNome] = React.useState("");
   const [campoSobrenome, setCampoSobrenome] = React.useState("");
   const [campoCPF, setcampoCPF] = React.useState("");
+  const [campoAceitaTermos, setCampoAceitaTermos] = React.useState(false);
+  const [campoEsporte, setCampoEsporte] = React.useState("");
 
   const salvar = () => {
-    console.log("Salvar:", { campoNome, campoSobrenome, campoCPF });
+    console.log("Salvar:", {
+      campoNome,
+      campoSobrenome,
+      campoCPF,
+      campoAceitaTermos,
+      campoEsporte,
+    });
   };
 
   return (
     <View style={estilos.container}>
       <View style={{ alignSelf: "center" }}>
-        <FontAwesome name="form" style={{ fontSize: 48, color: "red" }} />
+        <FontAwesome
+          name="file-text-o"
+          style={{ fontSize: 48, color: "gray" }}
+        />
       </View>
 
       <CampoTextoCustomizado
@@ -36,6 +51,28 @@ const TelaFormulario = () => {
         value={campoCPF}
         onChangeText={setcampoCPF}
       />
+
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={campoAceitaTermos ? "#f5dd4b" : "#f4f3f4"}
+        onValueChange={setCampoAceitaTermos}
+        value={campoAceitaTermos}
+      />
+
+      <RNPickerSelect
+        style={pickerSelectStyles}
+        onValueChange={setCampoEsporte}
+        value={campoEsporte}
+        items={[
+          { label: "Futebol", value: "1" },
+          { label: "Baseball", value: "2" },
+          { label: "Volei", value: "3" },
+          { label: "Surf", value: "4" },
+          { label: "Sakate", value: "5" },
+        ]}
+        placeholder={{ label: "Selecione um Esporte", value: null }}
+      />
+
       <BotaoCustomizado onPress={salvar}>Salvar</BotaoCustomizado>
     </View>
   );
